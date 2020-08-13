c3012e633d3d5e1dd9e8a46c300e393e51d95d47
edumetz16 <eduardo@linead.com.ar>
2020-08-13 11:24:39 -0300

    changes merge videos

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -105,9 +105,9 @@ left:0;
                 <li class="menu-item-parent {{ Request::is('/') ? 'active' : '' }}"> <a href="/">Home</a> </li>
                 <li class="menu-item-parent"> <a href="/#deportes1">Deportes</a> </li>
                 <li class="menu-item-parent {{ Request::is('noticias') ? 'active' : '' }}"> <a href="{{ url('/noticias' )}}">Noticias</a> </li>
-              <li class="menu-item-parent {{ Request::is('/videos') ? 'active' : '' }}"> <a href="/videos">Videos</a> </li>
+              <li class="menu-item-parent {{ Request::is('videos') ? 'active' : '' }}"> <a href="/videos">Videos</a> </li>
                 <!--<li class="menu-item-parent"> <a href="">Redes</a> </li>-->
-                <li class="menu-item-parent {{ Request::is('/perfiles') ? 'active' : '' }}"> <a href="{{route('perfiles')}}">#Perfilesxeneixes</a> </li>
+                <li class="menu-item-parent {{ Request::is('perfiles') ? 'active' : '' }}"> <a href="{{route('perfiles')}}">#Perfilesxeneixes</a> </li>
                 <li class="menu-item-parent"> <a href="/#momentos1">Momentos</a> </li>
                 <li class="menu-item-parent redes"> <a class="twitter" target="_blank" href=""><i class="fab fa-twitter"></i></a> </li>
               </ul>
