fa2dad043c4c5c11cec9ef7161abb779f4094f47
edumetz16 <eduardo@linead.com.ar>
2020-08-14 19:02:38 -0300

    changes merge videos

--- resources/views/archivev.blade.php
+++ resources/views/archivev.blade.php
@@ -41,7 +41,7 @@
                     <h6 class=""> {{$item->lead}}  </h6>
                   <h5 class="card-title"><a href="{{url('/videos')}}/{{$item->id}}">{{$item->title}}</a></h5>
                     <p class="card-text">{{$item->excerpt}}</p>
-                    <small class="text-muted"><i class="fas fa-calendar-alt mr-1 "></i>{{$item->created_at->format('d F Y')}} </small> <a class="more-news" href="{{url('/videos')}}/{{$item->id}}">Ver más..</a> </div>
+                     <a class="more-news" href="{{url('/videos')}}/{{$item->id}}">Ver más..</a> </div>
                 </div>
               </div></div>@endforeach
               
