843305c4c176a9f0081daab3723e8412f93e849a
edumetz16 <eduardo@linead.com.ar>
2020-08-24 17:36:48 -0300

    ajustes card videos

--- resources/views/archivep.blade.php
+++ resources/views/archivep.blade.php
@@ -72,7 +72,7 @@
         <div class="card"> <img class="card-img-top" src="{{asset('/storage')}}/{{$video->image}}" alt="">
           <div class="card-body text-center"> <span class="ply-btn medium"><i class="fa fa-play"></i></span>
             <h1>{{$video->title}}</h1>
-            <h3>Superclásico</h3>
+            <!-- <h3>Superclásico</h3> -->
           </div>
         </div>
         </a></div>
