02f75eb0b3809541dbf8a184ad68d24bfac7b7c7
edumetz16 <eduardo@linead.com.ar>
2020-08-27 14:54:23 -0300

    changes

--- resources/views/presingle.blade.php
+++ resources/views/presingle.blade.php
@@ -3,13 +3,10 @@
 
 <main role="main">
 	
-	var_dump($news);
+	<?php var_dump($news); ?>
 	
     <section class="page-banner">
-    <!-- @if ($news->cover_photo)
-    <div class="banner-img banner-fade animated scaleOut" style="background-image: url('{{Voyager::image($news->cover_photo)}}');"></div>
-    @else
-    @endif -->
+    
     <div class="banner-img banner-fade animated scaleOut" style="background-image: url('{{Voyager::image($news->image)}}');"></div>
     </section>
       
