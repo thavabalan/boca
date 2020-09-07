827ad6e830dd16e46151cfe4547225ef7b9e9c65
edumetz16 <eduardo@linead.com.ar>
2020-09-07 16:35:06 -0300

    merge

--- public/assets/js/mega_menu.js
+++ public/assets/js/mega_menu.js
@@ -299,8 +299,8 @@
                     var $window = $(window),
                         scrollFlag = true,
                         scrollFlagMobile = true,
-                        logoHeight = $('.menu-logo').outerHeight();
-                        //logoHeight=71;
+                        //logoHeight = $('.menu-logo').outerHeight();
+                        logoHeight=71;
                         if(!$('#header').hasClass('logo-center')){  
                             $('.mega-menu .menu-list-items').height(logoHeight);
                         }
