53fdb5d24c0f1b0e05f76d344876b8e7ac58630f
edumetz16 <eduardo@linead.com.ar>
2020-09-07 16:08:08 -0300

    merge

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -28,47 +28,50 @@ class PageController extends Controller
         $news = News::latest()->paginate(12);
         $videos = Video::latest()->paginate(20);
         $twittes = Twitter::getListStatuses(['list_id' => '1296500355958743045', 'count' => 10, 'format' => 'array', 'include_rts'=> false]);
-
+        $sports = Sport::orderBy('title')->get();
         $perfilesxeneizes = Perfilesxeneize::all();
-        return view('archive')->withNews($news)->withVideos($videos)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
+        return view('archive')->withNews($news)->withVideos($videos)->withSports($sports)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
     }
     public function parchive(){
         $news = News::latest()->paginate(20);
         $videos = Video::latest()->paginate(20);
         $perfilesxeneizes = Perfilesxeneize::latest()->paginate(12);
+        $sports = Sport::orderBy('title')->get();
         $twittes = Twitter::getListStatuses(['list_id' => '1296500355958743045', 'count' => 10, 'format' => 'array', 'include_rts'=> false]);
-        return view('archivep')->withNews($news)->withVideos($videos)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
+        return view('archivep')->withNews($news)->withVideos($videos)->withSports($sports)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
     }
     public function videoarchive(){
         $videos = Video::latest()->paginate(20);
         $twittes = Twitter::getListStatuses(['list_id' => '1296500355958743045', 'count' => 10, 'format' => 'array', 'include_rts'=> false]);
         $perfilesxeneizes = Perfilesxeneize::all();
-        return view('archivev')->withVideos($videos)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
+        $sports = Sport::orderBy('title')->get();
+        return view('archivev')->withVideos($videos)->withSports($sports)->withPerfilesxeneizes($perfilesxeneizes)->withTwittes($twittes);
     }
     public function singlenews($slug){
         $news = News::where('slug', $slug)
 
         ->first();
         $newses = News::latest()->paginate(20);
-
-        return view('newssingle')->withNews($news)->withNewses($newses);
+        $sports = Sport::orderBy('title')->get();
+        return view('newssingle')->withNews($news)->withNewses($newses)->withSports($sports);
     }
     public function singlepref($slug){
         $news = Perfilesxeneize::where('slug', $slug)->first();
         $newses = Perfilesxeneize::paginate(20);
-
-        return view('presingle')->withNews($news)->withNewses($newses);
+        $sports = Sport::orderBy('title')->get();
+        return view('presingle')->withNews($news)->withNewses($newses)->withSports($sports);
     }
     public function singlesport($id){
         $sport = Sport::find($id);
         $newses = News::latest()->paginate(20);
         $videos = Video::latest()->paginate(20);
-        return view('sposingle')->withSport($sport)->withNewses($newses)->withVideos($videos);
+        $sports = Sport::orderBy('title')->get();
+        return view('sposingle')->withSport($sport)->withNewses($newses)->withVideos($videos)->withSports($sports);
     }
     public function singlevideo($id){
         $video = Video::latest()->find($id);
         $videos = Video::latest()->paginate(20);
-
-        return view('videosingle')->withVideo($video)->withVideos($videos);
+        $sports = Sport::orderBy('title')->get();
+        return view('videosingle')->withVideo($video)->withVideos($videos)->withSports($sports);
     }
 }
