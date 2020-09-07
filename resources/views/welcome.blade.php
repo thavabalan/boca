1a177d10172497db9838ea8f00e49d439e4a7a50
edumetz16 <eduardo@linead.com.ar>
2020-09-07 16:40:58 -0300

    merge

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -116,7 +116,7 @@ y un camino a la competencia de élite</h2>
       </div>
       <div class="owl-carousel owl-theme" id="videos" >
         @foreach($videos->slice(0, 3) as $video)
-        <a data-lity href="#inline{{$video->id}}">
+        <a href="#inline{{$video->id}}">
         <div class="card"> <img class="card-img-top" src="{{asset('/storage')}}/{{$video->image}}" alt="">
           <div class="card-body text-center"> <span class="ply-btn medium"><i class="fa fa-play"></i></span>
             <h1>{{$video->title}}</h1>
