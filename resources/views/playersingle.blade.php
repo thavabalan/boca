99e37f99af4498fd793bb4bc3cddb0ebf5aeef8a
edumetz16 <eduardo@linead.com.ar>
2020-09-15 17:39:57 -0300

    planteles

--- resources/views/playersingle.blade.php
+++ resources/views/playersingle.blade.php
@@ -6,7 +6,7 @@
 	
 	
     <section class="page-banner">
-    <h1 class="team-title">{{$team->title}}</h1>
+    <!-- <h1 class="team-title">{{$team->title}}</h1> -->
     <div class="banner-img banner-fade animated scaleOut" style="background-image: url('{{Voyager::image($team->featured_image)}}');"></div>
     </section>
       
